7e4e16dce5437c4cc4719fe98de3680582c9e9f8
Kan3103 <khang310304@gmail.com>
2023-11-24 16:41:59 +0700

    Update model f(x)

--- PPT.py
+++ PPT.py
@@ -1,18 +1,19 @@
 import os 
 import pandas as pd
 import matplotlib.pyplot as plt
+from sympy import symbols, Eq, solve
 os.system("cls")
 
-def p_x(p_xk):
-    p_xk=0
-    
-def q_x(q_xk):
-    q_xk=0
+def linear_function(x, a, b):
+    return a * x + b
 
 def main():
     name_file=input("Enter the path of file: ")
     file_excel = pd.read_excel(name_file) # Enter the true hyperlink to read excel file
-
+    year = pd.DataFrame(file_excel,columns=['Year'])
+    average= pd.DataFrame(file_excel,columns=['World Average Daily Production of Crude Oil includeing lease condensate (Mb/d)'])
+    cumulative = pd.DataFrame(file_excel,columns=['Cumulative World Production Since 1980 (Mb)'])
+    
     print("1 to print in the data in 2D")
     print("2 to Math modeling the data in your selection")
     print("3 to solve the f(x),f'(x) ,f\"(x) from 2 ")
@@ -21,10 +22,6 @@ def main():
     num=int(input("Enter a number: "))
 
     if (num==1) :
-        year = pd.DataFrame(file_excel,columns=['Year'])
-        average= pd.DataFrame(file_excel,columns=['World Average Daily Production of Crude Oil includeing lease condensate (Mb/d)'])
-        cumulative = pd.DataFrame(file_excel,columns=['Cumulative World Production Since 1980 (Mb)'])
-        
         fig, ax1 = plt.subplots()
         ax1.plot(year, average, linestyle='-', color='r', label='Average')
         ax1.set_xlabel('Năm')
@@ -42,6 +39,35 @@ def main():
         ax2.legend(loc='upper right')
 
         plt.show()
-
+    if(num==2) :
+        print("Enter 1 for x")
+        print("Enter 2 for x^2")
+        print("Enter 3 for x^3")
+        print("Enter 4 for a^x")
+        select_model= int(input("Enter your selection: "))
+        
+        if( select_model == 1 ):
+            a= year["Year"].shape[0]
+            b=c=d=e=0
+            for i in range(0,a):
+                b+=year["Year"][i]
+                c+=cumulative["Cumulative World Production Since 1980 (Mb)"][i]
+                d+=year["Year"][i]**2
+                e+=year['Year'][i]*cumulative['Cumulative World Production Since 1980 (Mb)'][i]
+                print(a,b,c,d,e)
+            x, y = symbols('x y')
+            # Khai báo phương trình
+            eq1 = Eq(a*x + b*y, c)
+            eq2 = Eq(b*x + d*y, e)
+            solution = solve((eq1, eq2), (x, y))
+            y_values=linear_function(year,solution[y],solution[x])
+            print(solution[x] , "+" ,solution[y],"x")   
+            plt.plot(year,y_values, color="red" , label='Model')
+            plt.plot(year,cumulative, color="blue",label='Data')
+            plt.xlabel('Year')
+            plt.ylabel('Cumulative World Production Since 1980 (Mb)')
+            plt.title('Linear Model vs. Data')
+            plt.legend()
+            plt.show()
         
 main()
